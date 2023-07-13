5cca7cefd236775a22cf092cb17f5ee48f0451a8
Fernando Perez <fperez4085@gmail.com>
2023-07-13 16:57:31 -0300

    Proyecto ferreteria

--- js/aap.js
+++ js/aap.js
@@ -11,37 +11,49 @@ function mostrarProductos(itemsProductos) {
         <p class="precio">${el.precio}</p>
         <button id="${el.id}" class="button__agregar">Comprar</button>
         </div>`;
-    contenedorCard.appendChild(card);
+        contenedorCard.appendChild(card);
     });
     const bottonCompra = document.querySelectorAll(".button__agregar");
-    bottonCompra.forEach(item =>{
-        item.addEventListener("click", (e)=>{
+    bottonCompra.forEach(item => {
+        item.addEventListener("click", (e) => {
             agregarAlCarrito(e.target.id)
         });
     });
 }
-function detalleProducto (){
-    const contenedorCardDos = document.getElementById("detalle-producto")
+const contenedorCardDos = document.getElementById("detalle-producto");
+function detalleProducto() {
+    carritoArray.forEach(prod => {
+        const cardDos = document.createElement("div");
+        contenedorCardDos.innerHTML = `
+        <div class="main__detalles">
+        <p class="product__description">${prod.nombre}</p>
+        <p class="precio">${prod.precio}</p>
+        <button id="${prod.id}" class="button__agregar">Cancelar</button>
+        </div>`;
+        contenedorCardDos.appendChild(cardDos);
+    })
 }
 
 
-function agregarAlCarrito (id){
-    let productoEnLista = productos.find(prod =>prod.id === parseInt(id));
+function agregarAlCarrito(id) {
+    let productoEnLista = productos.find(prod => prod.id === parseInt(id));
     carritoArray.push(productoEnLista);
     console.log(carritoArray);
     storage(carritoArray);
 
 }
 
-function storage (items){
+function storage(items) {
     localStorage.setItem("producto", JSON.stringify(items));
-    const productoSeleccionado = carritoArray.reduce((acc, el)=> acc += `${el.nombre} -$ ${el.precio}\n`, "");
+    const productoSeleccionado = carritoArray.reduce((acc, el) => acc += `${el.nombre} -$ ${el.precio}\n`, "");
     alert(productoSeleccionado);
 
 }
 
 
-mostrarProductos(productos); 
+mostrarProductos(productos);
+detalleProducto()
+
 const carritoArray = JSON.parse(localStorage.getItem("producto")) || [];
 
 
