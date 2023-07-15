440b919dc06a660fa096b7e20796dafd79d6ded4
Fernando Perez <fperez4085@gmail.com>
2023-07-15 12:34:29 -0300

    Proyecto Ferreteria

--- js/aap.js
+++ js/aap.js
@@ -7,8 +7,8 @@ function mostrarProductos(itemsProductos) {
         card.setAttribute("class", "main__item__productos")
         card.innerHTML = `<img class="producto__img img--product" src="${el.img}" alt="${el.nombre}">
         <div  class="main__detalles">
-        <p class="product__description">${el.nombre}</p>
-        <p class="precio">$${el.precio}</p>
+        <p class="product__description"></p>
+        <p class="precio"></p>
         <button id="${el.id}" class="button__agregar">ver detalle</button>
         </div>`;
         contenedorCard.appendChild(card);
@@ -21,7 +21,7 @@ function mostrarProductos(itemsProductos) {
 
 function mostrarDetalle(e) {
     const contenedorDetalle = document.getElementById("detalle-producto");
-    contenedorDetalle.innerHTML=``;
+    contenedorDetalle.innerHTML = ``;
     console.log(contenedorDetalle);
     const id = parseInt(e.target.id);
     const productoEnContrado = productos.find(p => p.id === id);
@@ -33,14 +33,38 @@ function mostrarDetalle(e) {
     <p class="product__description">${productoEnContrado.nombre}</p>
     <p>$${productoEnContrado.precio}</p>
     <button id="${productoEnContrado.id}" class="button__detalles">Comprar</button>
-    <button  class="button__detalles">Cancelar</button>
+    <button  class="button__detalles none">Cancelar</button>
     `;
     contenedorDetalle.appendChild(cardDos);
 
     const btnComprar = document.querySelector(".button__detalles");
     console.log(btnComprar);
-    btnComprar.addEventListener("click", ()=>alert(`Felicidades realizaste la compra de ${productoEnContrado.nombre}`));
+    btnComprar.addEventListener("click", () => alert(`Felicidades realizaste la compra de ${productoEnContrado.nombre}`));
+    const btnCancelar = document.getElementsByClassName(".none");
+    btnCancelar.addEventListener("click", (e)=>{
+        eliminarProducto(cardDos, id);
+    })
 }
+
+
+function eliminarProducto(carrito, idProducto) {
+    const indice = carrito.findIndex(producto => producto.id === idProducto);
+    if (indice !== -1) {
+        carrito.splice(indice, 1);
+        console.log(`El producto con ID ${idProducto} ha sido eliminado del carrito.`);
+    } else {
+        console.log(`No se encontró ningún producto con ID ${idProducto} en el carrito.`);
+    }
+} 
+/* function eliminarProductos(id) {
+    carrito = carrito.filter((producto) => producto.id !== id)
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+    mostrarCarrito()
+} */
+
+
+
+
 
 
 const carritoArray = JSON.parse(localStorage.getItem("producto")) || [];
@@ -57,4 +81,3 @@ function agregarAlCarrito(e) {
 
 
 mostrarProductos(productos);
-
